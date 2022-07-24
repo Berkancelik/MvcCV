ebade96d919dd248c6d87fc9e1b3078d4de5b3d7
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-24 20:45:12 +0300

    Entity folder added

--- MvcCV/Controllers/DefaultController.cs
+++ MvcCV/Controllers/DefaultController.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using MvcCV.Models
 namespace MvcCV.Controllers
 {
     public class DefaultController : Controller
     {
+        DbCvContext dbCvContext;
         public IActionResult Index()
         {
             return View();
