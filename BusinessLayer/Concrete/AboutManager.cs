40f60148c9090cae65a28be0e36b6a80d79d6030
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-25 14:20:27 +0300

    DataAccesLayer Abstract interfaces implemented : IGenericDal

--- BusinessLayer/Concrete/AboutManager.cs
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using MvcCV.EntiyLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class AboutManager
+    public class AboutManager : IAboutService
     {
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DataAccessLayer/Abstract/ISkillDal.cs
+++ DataAccessLayer/Abstract/ISkillDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcCV.EntiyLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ISkillDal
+    public interface ISkillDal:IGenericDal<Skill>
     {
     }
 }
