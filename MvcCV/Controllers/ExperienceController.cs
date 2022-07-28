6ee2fc0579a3ae3dfadef3d2030d65176a918416
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-28 10:55:53 +0300

    AddExperience added

--- MvcCV/Controllers/ExperienceController.cs
+++ MvcCV/Controllers/ExperienceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCV.EntiyLayer.Concrete;
 using MvcCV.Repositories;
 
 namespace MvcCV.Controllers
@@ -10,6 +11,19 @@ namespace MvcCV.Controllers
         {
             var values = repo.List();
             return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience(Experience p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
