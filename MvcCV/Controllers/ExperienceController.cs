3910d9b2e456dd072a352b1b3a612c7138aaa4c3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-28 10:47:01 +0300

    Experience fixed

---
+++ MvcCV/Controllers/ExperienceController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCV.Repositories;
+
+namespace MvcCV.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        ExperienceRepository repo = new ExperienceRepository();
+        public IActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+    }
+}
